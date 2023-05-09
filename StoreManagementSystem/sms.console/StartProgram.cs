506ae9098c7e24a3b61a616772b757308cc0ff63
Polya <PDDimitrova19@codingburgas.bg>
2023-05-09 21:18:34 +0300

    Add options when starting the program

---
+++ StoreManagementSystem/sms.console/StartProgram.cs
@@ -0,0 +1,30 @@
+﻿namespace sms.console
+{
+    internal class StartProgram
+    {
+        public static void Print()
+        {
+            Console.Clear();
+            Console.WriteLine("[L] - Login   [R] Register   [E] Exit");
+
+            while(true) {
+                var input = char.ToUpper(Console.ReadKey().KeyChar);
+
+                switch(input)
+                {
+                    case 'L':
+                        LoginMenu loginMenu = new LoginMenu(); // create an instance of the LoginMenu class
+                        loginMenu.Print(); break;
+                    case 'R': /*RegisterMenu.Print();*/ break;
+                    case 'E': Environment.Exit(0); break;
+                    default: break;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Print();
+        }
+    }
+}
