e2bca55ad2d58bbad25384c70848cb1265ff2866
Polya <PDDimitrova19@codingburgas.bg>
2023-05-28 16:20:54 +0300

    Create HashPassword method

--- src/StoreManagementSystem/sms.bll/UserService.cs
+++ src/StoreManagementSystem/sms.bll/UserService.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using sms.dal.Data;
+using sms.dal.Models;
+using sms.dal.Repositories;
+using System.Security.Cryptography;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +10,49 @@ using System.Threading.Tasks;
 
 namespace sms.bll
 {
-    internal class UserService
+    public class UserService
     {
+        public static User? GetUserByUsername(string username)
+        {
+            using (var context = new StoreManagementSystemContext())
+            {
+                UserRepository userRepository = new(context);
 
+                User? user = userRepository.GetUsers().FirstOrDefault(u => u.Username == username);
+
+                return user;
+            }
+        }
+
+        public static bool LoginUser(string username, string password)
+        {
+            using (var context = new StoreManagementSystemContext())
+            {
+                UserRepository userRepository = new(context);
+
+                User user = GetUserByUsername(username);
+
+                if (user != null)
+                {
+                    string hashedPassword = HashPassword(password);
+                    if (user.Password == hashedPassword)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        public static string HashPassword(string password)
+        {
+            SHA256 hash = SHA256.Create();
+
+            byte[] passBytes = Encoding.Default.GetBytes(password);
+
+            string hashedPass = Convert.ToHexString(hash.ComputeHash(passBytes));
+
+            return hashedPass;
+        }
     }
 }
--- src/StoreManagementSystem/sms.console/LoginMenu.cs
+++ src/StoreManagementSystem/sms.console/LoginMenu.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using sms.bll;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
 
 namespace sms.console
 {
@@ -22,12 +24,33 @@ namespace sms.console
             Console.Write("\nPassword: ");
 
             string password = GetPassword();
-            
+
+            if (!UserService.LoginUser(username, password))
+            {
+                Console.WriteLine();
+                Console.Write("Wrong Username or Password");
+                Console.ReadKey();
+                Print();
+            }
+
+            Console.WriteLine();
+            Console.Write("User Logged In");
+            Console.ReadKey(true);
+            StartMenu.Print();
+
         }
 
         private static string GetUsername()
         {
             string username = Console.ReadLine();
+
+            if (username.IsNullOrEmpty())
+            {
+                Console.WriteLine();
+                Console.Write("Username must be inputed");
+                Console.ReadKey();
+                Print();
+            }
 
             return username;
         }
@@ -35,6 +58,14 @@ namespace sms.console
         private static string GetPassword()
         {
             string password = Console.ReadLine();
+
+            if (password.IsNullOrEmpty())
+            {
+                Console.WriteLine();
+                Console.Write("Password must be inputed");
+                Console.ReadKey();
+                Print();
+            }
 
             return password;
         }
